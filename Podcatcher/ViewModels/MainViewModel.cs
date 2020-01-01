db1474a1e731b50d291db25bb20baefd8aee9cf4
Lee Scholefield <leescholefield@gmail.com>
2020-01-01 10:09:23 +0000

    Added ListBox for podcast episodes

--- Podcatcher/ViewModels/MainViewModel.cs
+++ Podcatcher/ViewModels/MainViewModel.cs
@@ -25,7 +25,22 @@
                 {
                     Title = "Revolutions",
                     Author = "Mike Duncan",
-                    ImageUrl = "http://static.libsyn.com/p/assets/3/4/5/f/345fbd6a253649c0/RevolutionsLogo_V2.jpg"
+                    ImageUrl = "http://static.libsyn.com/p/assets/3/4/5/f/345fbd6a253649c0/RevolutionsLogo_V2.jpg",
+                    Episodes = new System.Collections.Generic.List<Models.Episode>
+                    {
+                        new Models.Episode
+                        {
+                            Title = "FIRST",
+                            Author = "Mike Duncan",
+                            Description = "asda  sd a sd gsd f s fdasd as as d asd  asd a sda sd asd sd f dfg fgh fh ghfg"
+                        },
+                        new Models.Episode
+                        {
+                            Title = "Second",
+                            Author = "Mike Duncan",
+                            Description = "asdas  sdfg a sd a asd as a sd asd a sd as da sd as das d as dsa d a"
+                        }
+                    }
 
                 }
             };
