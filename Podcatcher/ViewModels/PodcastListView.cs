a6a3f13a181b133be6d30a2a8ba181e670068bac
Lee Scholefield <leescholefield@gmail.com>
2020-01-07 11:30:44 +0000

    Added ViewModel for displaying a list of Podcasts

---
+++ Podcatcher/ViewModels/PodcastListView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Podcatcher.Models;
+using Podcatcher.ViewModels.Commands;
+
+namespace Podcatcher.ViewModels
+{
+    /// <summary>
+    /// ViewModel for displaying a list of <see cref="Podcast"/>
+    /// </summary>
+    public class PodcastListViewModel : BaseViewModel
+    {
+
+        #region Properties
+
+        public List<Podcast> _podcasts;
+        public List<Podcast> Podcasts
+        {
+            get
+            {
+                return _podcasts;
+            }
+            set
+            {
+                _podcasts = value;
+                OnPropertyChanged("Podcasts");
+
+
+            }
+        }
+
+        /// <summary>
+        /// Opens a <see cref="PodcastViewModel"/> with the passed Podcast.
+        /// </summary>
+        public ICommand DisplayPodcastCommand { get; set; }
+
+        /// <summary>
+        /// Adds the given Podcast to list of Subscriptions />
+        /// </summary>
+        public ICommand SubscribeToPodcast { get; set; }
+
+        #endregion
+
+        protected override void InitializeCommands()
+        {
+            DisplayPodcastCommand = new RelayCommand<Podcast>(DisplayPodcastCommand_Execute);
+            SubscribeToPodcast = new RelayCommand<Podcast>(SubscribeToPodcast_Execute);
+        }
+
+        private void DisplayPodcastCommand_Execute(Podcast podcast)
+        {
+
+        }
+
+        private void SubscribeToPodcast_Execute(Podcast podcast)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
