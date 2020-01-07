2b4f5fc3b9b8c9fdc97ee777badb1e82a8d0eaf2
Lee Scholefield <leescholefield@gmail.com>
2020-01-07 18:04:12 +0000

    Added ServiceLocator service

---
+++ Podcatcher/ViewModels/Services/IServiceLocator.cs
@@ -0,0 +1,13 @@
+﻿namespace Podcatcher.ViewModels.Services
+{
+    public interface IServiceLocator
+    {
+
+        /// <summary>
+        /// Returns an instance of <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Service to return.</typeparam>
+        /// <returns>An instance of <typeparamref name="T"/>, if it has been registered.</returns>
+        T GetService<T>();
+    }
+}
---
+++ Podcatcher/ViewModels/Services/ServiceLocator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Podcatcher.ViewModels.Services
+{
+    public class ServiceLocator : IServiceLocator
+    {
+        private readonly IDictionary<object, object> registeredServices;
+
+        private static ServiceLocator _instance;
+
+        public static ServiceLocator Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ServiceLocator();
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Private constructor to prevent more than one instance of this class being created.
+        /// </summary>
+        private ServiceLocator()
+        {
+            registeredServices = new Dictionary<object, object>
+            {
+                
+            };
+
+        }
+
+        /// <summary>
+        /// Returns an instance of <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of service to return. Note, this will be the interface type, not the implementation type.</typeparam>
+        /// <returns>An instance of type T.</returns>
+        public T GetService<T>()
+        {
+            try
+            {
+                return (T)registeredServices[typeof(T)];
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new ApplicationException("The requested service has not been registered. Service name = " + typeof(T));
+            }
+        }
+    }
+}
+}
