9524de1533ff557dba563b3fda3a9a0a0549711c
Lee Scholefield <leescholefield@gmail.com>
2020-01-07 18:12:44 +0000

    Register navigation commands with NavigationService

--- Podcatcher/ViewModels/MainViewModel.cs
+++ Podcatcher/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace Podcatcher.ViewModels
+﻿using Podcatcher.Models;
+using Podcatcher.ViewModels.Services;
+
+namespace Podcatcher.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
@@ -19,31 +22,37 @@
 
         public MainViewModel()
         {
-            MainView = new PodcastViewModel
-            {
-                Podcast = new Models.Podcast
-                {
-                    Title = "Revolutions",
-                    Author = "Mike Duncan",
-                    ImageUrl = "http://static.libsyn.com/p/assets/3/4/5/f/345fbd6a253649c0/RevolutionsLogo_V2.jpg",
-                    Episodes = new System.Collections.Generic.List<Models.Episode>
-                    {
-                        new Models.Episode
-                        {
-                            Title = "FIRST",
-                            Author = "Mike Duncan",
-                            Description = "asda  sd a sd gsd f s fdasd as as d asd  asd a sda sd asd sd f dfg fgh fh ghfg"
-                        },
-                        new Models.Episode
-                        {
-                            Title = "Second",
-                            Author = "Mike Duncan",
-                            Description = "asdas  sdfg a sd a asd as a sd asd a sd as da sd as das d as dsa d a"
-                        }
-                    }
+            RegisterNavigationCommands();
 
-                }
+            // just for testing
+            MainView = new PodcastListViewModel
+            {
+               Podcasts = new System.Collections.Generic.List<Models.Podcast>
+               {
+                   new Models.Podcast
+                   {
+                       Title = "Revolutions",
+                       Author = "Mike Duncan",
+                       ImageUrl = "https://ssl-static.libsyn.com/p/assets/3/4/5/f/345fbd6a253649c0/RevolutionsLogo_V2.jpg"
+                   },
+                   new Models.Podcast
+                   {
+                       Title = "Risky Business",
+                       Author = "Risky.Biz",
+                       ImageUrl = "https://risky.biz/static/img/rbipod2.jpg"
+                   }
+               }
             };
+        }
+
+        private void RegisterNavigationCommands()
+        {
+            var navService = ServiceLocator.Instance.GetService<INavigationService>();
+            navService.Register<PodcastViewModel>( (t, args) =>
+            {
+                MainView = new PodcastViewModel() { Podcast = (Podcast)args[0] };
+                return true;
+            });
         }
     }
 }
