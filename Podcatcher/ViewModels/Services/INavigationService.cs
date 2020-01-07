5fe22375d50c3d3aa3b18cf387ab274a468def79
Lee Scholefield <leescholefield@gmail.com>
2020-01-07 18:07:25 +0000

    Added NavigationService service

---
+++ Podcatcher/ViewModels/Services/INavigationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Podcatcher.ViewModels.Services
+{
+    /// <summary>
+    /// Responsible for navigating between different ViewModels.
+    /// </summary>
+    public interface INavigationService
+    {
+        /// <summary>
+        /// Registers a function to execute when <see cref="NavigateTo"/> is called.
+        /// </summary>
+        /// <typeparam name="T">The ViewModel to open</typeparam>
+        /// <param name="func">Function that opens the view model.</param>
+        void Register<T>(Func<Type, object[], bool> func) where T : BaseViewModel;
+
+        /// <summary>
+        /// Executes the function that navigates to <typeparamref name="T"/>. If no function was registered to T this will throw an Exception.
+        /// </summary>
+        /// <typeparam name="T">ViewModel to navigate to.</typeparam>
+        /// <param name="optionalParams">Any addional arguments required for opening the ViewModel.</param>
+        void NavigateTo<T>(params object[] optionalParams) where T : BaseViewModel;
+
+        /// <summary>
+        /// Executes the function that navigates to <typeparamref name="T"/>. If no function was registered to T this will throw an Exception.
+        /// </summary>
+        /// <typeparam name="T">ViewModel to navigate to.</typeparam>
+        /// <param name="optionalParams">Any addional arguments required for opening the ViewModel.</param>
+        void NavigateTo(BaseViewModel viewModel, params object[] optionalParams);
+
+        /// <summary>
+        /// Navigates back to the previous ViewModel.
+        /// </summary>
+        void Previous();
+    }
+}
---
+++ Podcatcher/ViewModels/Services/NavigationService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Podcatcher.ViewModels.Services
+{
+    public class NavigationService : INavigationService
+    {
+
+        #region Properties
+
+        private readonly Dictionary<Type, Func<Type, object[], bool>> recieverMap;
+
+        private Type previousViewModel;
+        private object[] previousViewModelArgs;
+        private Type currentViewModel;
+        private object[] currentViewModelArgs;
+
+        private static NavigationService instance;
+        public static NavigationService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new NavigationService();
+                }
+
+                return instance;
+
+            }
+        }
+
+        #endregion
+
+        public NavigationService()
+        {
+            recieverMap = new Dictionary<Type, Func<Type, object[], bool>>();
+
+        }
+
+        public void NavigateTo<T>(params object[] optionalParams) where T : BaseViewModel
+        {
+            Type targetType = typeof(T);
+            if (!recieverMap.ContainsKey(targetType))
+            {
+                throw new Exception("No function has been registered for viewmodel of type " + targetType);
+            }
+
+            if (currentViewModel != null)
+            {
+                previousViewModel = currentViewModel;
+                previousViewModelArgs = currentViewModelArgs;
+            }
+
+            currentViewModel = targetType;
+            currentViewModelArgs = optionalParams;
+
+            var f = recieverMap[targetType];
+            f(targetType, optionalParams);
+        }
+
+        public void NavigateTo(BaseViewModel viewModel, params object[] optionalParams)
+        {
+            Type targetType = viewModel.GetType();
+            if (!recieverMap.ContainsKey(targetType))
+            {
+                throw new Exception("No function has been registered for viewmodel of type " + targetType);
+            }
+
+            if (currentViewModel != null)
+            {
+                previousViewModel = currentViewModel;
+                previousViewModelArgs = currentViewModelArgs;
+            }
+
+            currentViewModel = targetType;
+            currentViewModelArgs = optionalParams;
+
+            var f = recieverMap[targetType];
+            f(targetType, optionalParams);
+        }
+
+        public void Register<T>(Func<Type, object[], bool> func) where T : BaseViewModel
+        {
+
+            recieverMap.Add(typeof(T), func);
+        }
+
+        public void Previous()
+        {
+            if (previousViewModel == null)
+            {
+                throw new InvalidOperationException("No previous viewmodel is registered");
+            }
+
+            currentViewModel = previousViewModel;
+            currentViewModelArgs = previousViewModelArgs;
+
+            var f = recieverMap[previousViewModel];
+            f(previousViewModel, previousViewModelArgs);
+        }
+    }
+}
