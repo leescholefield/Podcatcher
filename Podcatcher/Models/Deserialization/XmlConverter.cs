63e48b188f53a92ec446684e3c9dcd358cd16c8e
Lee Scholefield <leescholefield@gmail.com>
2020-01-08 15:20:06 +0000

    Added ConvertEpisodes method for parsing just a list of episodes

--- Podcatcher/Models/Deserialization/XmlConverter.cs
+++ Podcatcher/Models/Deserialization/XmlConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace Podcatcher.Models.Deserialization
@@ -19,6 +20,14 @@ namespace Podcatcher.Models.Deserialization
             return ParseFeed(xml);
         }
 
+        public List<Episode> ConvertEpisodes(string xml)
+        {
+            if (xml == "")
+                return null;
+
+            return ParseEpisodes(xml);
+        }
+
         private Podcast ParseFeed(string xml)
         {
             XNamespace itunesNs = "http://www.itunes.com/dtds/podcast-1.0.dtd";
@@ -30,16 +39,7 @@ namespace Podcatcher.Models.Deserialization
             var title = channelRoot.Element("title").Value;
             var author = channelRoot.Element(itunesNs + "author").Value;
             var imageUrl = channelRoot.Element("image").Element("url").Value;
-
-            // episodes
-            var episodes = (from s in channelRoot.Descendants("item")
-                            select new Episode()
-                            {
-                                Title = s.Element("title").Value,
-                                Author = author,
-                                Description = s.Element("description").Value,
-                                StreamUrl = s.GetAttributeOfElement("enclosure", "url", nullValue:"")
-                            }).ToList();
+            var episodes = ParseEpisodes(channelRoot);
 
             return new Podcast
             {
@@ -49,6 +49,31 @@ namespace Podcatcher.Models.Deserialization
                 FeedUrl = feed,
                 Episodes = episodes
             };
+        }
+
+        private List<Episode> ParseEpisodes(string xml)
+        {
+            var doc = XDocument.Parse(xml);
+            var channelRoot = doc.Element("rss").Element("channel");
+
+            return ParseEpisodes(channelRoot);
+        }
+
+        private List<Episode> ParseEpisodes(XElement root)
+        {
+            XNamespace itunesNs = "http://www.itunes.com/dtds/podcast-1.0.dtd";
+            var author = root.Element(itunesNs + "author").Value;
+
+            var episodes = (from s in root.Descendants("item")
+                            select new Episode()
+                            {
+                                Title = s.Element("title").Value,
+                                Author = author,
+                                Description = s.Element("description").Value,
+                                StreamUrl = s.GetAttributeOfElement("enclosure", "url", nullValue: "")
+                            }).ToList();
+
+            return episodes;
         }
     }
 
