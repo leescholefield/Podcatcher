9a521935706846524d37ec505d142b2cb8d8cef8
Lee Scholefield <leescholefield@gmail.com>
2020-01-08 15:21:30 +0000

    Created service for parsing Rss feeds

---
+++ Podcatcher/ViewModels/Services/IRssDeserializer.cs
@@ -0,0 +1,21 @@
+﻿using Podcatcher.Models;
+using System.Collections.Generic;
+
+namespace Podcatcher.ViewModels.Services
+{
+    /// <summary>
+    /// Deserializes an RSS feed.
+    /// </summary>
+    public interface IRssDeserializer
+    {
+
+        /// <summary>
+        /// Deserializes an Rss feed into a <see cref="Podcast"/> instance.
+        /// </summary>
+        /// <param name="url">Url of the Rss feed to deserialize.</param>
+        /// <returns>A deserialized Podcast instance.</returns>
+        Podcast Deserialize(string url);
+
+        List<Episode> DeserializeEpisodes(string url);
+    }
+}
---
+++ Podcatcher/ViewModels/Services/RssDeserializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Podcatcher.Models;
+using Podcatcher.Models.Deserialization;
+
+namespace Podcatcher.ViewModels.Services
+{
+    public class RssDeserializer : IRssDeserializer
+    {
+        private readonly IPodcastDeserializer Deserializer;
+
+        public RssDeserializer(IPodcastDeserializer deserializer)
+        {
+            Deserializer = deserializer;
+        }
+
+        public Podcast Deserialize(string url)
+        {
+            return Deserializer.Deserialize(url);
+        }
+
+        public List<Episode> DeserializeEpisodes(string url)
+        {
+            return Deserializer.DeserializeEpisodes(url);
+        }
+    }
+}
