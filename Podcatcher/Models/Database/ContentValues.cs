f7f8363e707d06599d7e31df1fbf13f30054a68e
Lee Scholefield <leescholefield@gmail.com>
2020-01-13 15:39:49 +0000

    Created IDatabase interface for interacting with a SQL-based database. Created ContentValues as a wrapper for key value pairs

---
+++ Podcatcher/Models/Database/ContentValues.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Podcatcher.Models.Database
+{
+    public class ContentValues
+    {
+
+        private readonly Dictionary<string, object> valueDict;
+
+        public ContentValues()
+        {
+            valueDict = new Dictionary<string, object>();
+        }
+
+        public void Add(string key, object value)
+        {
+            valueDict.Add(key, value);
+        }
+    }
+}
---
+++ Podcatcher/Models/Database/IDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Podcatcher.Models.Database
+{
+    /// <summary>
+    /// Interface for a generic model-orientated Database
+    /// </summary>
+    /// <typeparam name="T">Class that this database can save / retrieve.</typeparam>
+    public interface IDatabase<T>
+    {
+
+        void Insert(T toInsert);
+
+        /// <summary>
+        /// Deletes the passed object from the database.
+        /// </summary>
+        void Delete(T toDelete);
+
+        /// <summary>
+        /// Deletes an object by id.
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(long id);
+
+        List<T> AllRecords();
+    }
+}
