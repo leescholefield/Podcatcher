13b5da80c2afcdc2b10098bd1fb7867bd8617121
Lee Scholefield <leescholefield@gmail.com>
2020-01-17 12:58:42 +0000

    Created View and ViewModel for searching itunes for a podcast

---
+++ Podcatcher/ViewModels/SearchViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Podcatcher.Models;
+using Podcatcher.ViewModels.Commands;
+using Podcatcher.ViewModels.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Podcatcher.ViewModels
+{
+    public class SearchViewModel : BaseViewModel
+    {
+
+        #region Properties
+
+        public ICommand SearchCommand { get; set; }
+
+        public string SearchTerm { get; set; }
+
+        private List<Podcast> _results = new List<Podcast>();
+        public List<Podcast> Results
+        {
+            get
+            {
+                return _results;
+            }
+            set
+            {
+                _results = value;
+                OnPropertyChanged("Results");
+            }
+        }
+
+
+        #endregion
+
+        protected override void InitializeCommands()
+        {
+            SearchCommand = new RelayCommand<string>(SearchCommand_Execute);
+        }
+
+        private void SearchCommand_Execute(string term)
+        {
+            var ser = ServiceLocator.Instance.GetService<IItunesSearchService>();
+            Results =  ser.Search(term);
+        }
+
+        private bool SearchCommand_CanExecute(string term)
+        {
+            return (term != null && term != "");
+        }
+    }
+}
