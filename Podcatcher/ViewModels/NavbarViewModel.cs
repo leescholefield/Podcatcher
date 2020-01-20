ca2eeb21d1440de35ea81a9e14b4defe7a19783f
Lee Scholefield <leescholefield@gmail.com>
2020-01-20 12:52:16 +0000

    Created ViewModel and View for a side navigation bar. App.xaml : registered new navbar

---
+++ Podcatcher/ViewModels/NavbarViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Podcatcher.ViewModels.Commands;
+using Podcatcher.ViewModels.Services;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace Podcatcher.ViewModels
+{
+    public class NavbarViewModel : BaseViewModel
+    {
+
+        #region Properties
+
+        private readonly INavigationService NavService;
+
+        public List<MenuItem> Items { get; set; }
+
+        public ICommand NavigateToViewModelCommand { get; set; }
+
+
+        #endregion
+
+        #region Initialization
+
+        public  NavbarViewModel() : this(ServiceLocator.Instance.GetService<INavigationService>())
+        {
+        }
+
+        public NavbarViewModel(INavigationService navService)
+        {
+            NavService = navService;
+            CreateMenuItems();
+        }
+
+        private void CreateMenuItems()
+        {
+            Items = new List<MenuItem>
+            {
+                new MenuItem
+                {
+                    DisplayName = "Search",
+                    ViewModel = new SearchViewModel()
+                }
+            };
+        }
+
+        protected override void InitializeCommands()
+        {
+            NavigateToViewModelCommand = new RelayCommand<MenuItem>(NavigateToViewModel_Execute);
+        }
+
+        #endregion
+
+        private void NavigateToViewModel_Execute(MenuItem item)
+        {
+            NavService.NavigateTo(item.ViewModel, item.OptionalParams);
+        }
+
+    }
+
+    public class MenuItem
+    {
+        public BaseViewModel ViewModel { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public object[] OptionalParams { get; set; }
+    }
+}
