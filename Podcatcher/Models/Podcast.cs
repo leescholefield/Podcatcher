213bb9759728545656669dc43b8daf812bcbbfc4
Lee Scholefield <leescholefield@gmail.com>
2020-01-21 12:40:13 +0000

    Podcast.cs : removed unused XML tags. Added Subscribed property, to indicate whether the user is subscibed to the podcast or not.

--- Podcatcher/Models/Podcast.cs
+++ Podcatcher/Models/Podcast.cs
@@ -1,23 +1,20 @@
 ﻿using System.Collections.Generic;
-using System.Xml.Serialization;
 
 namespace Podcatcher.Models
 {
-    [XmlRoot("channel")]
     public class Podcast
     {
 
-        [XmlElement("title")]
         public string Title { get; set; }
 
-        [XmlElement("itunes:author")]
         public string Author { get; set; }
 
         public string FeedUrl { get; set; }
 
-        [XmlAttribute("image", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
         public string ImageUrl { get; set; }
 
         public List<Episode> Episodes { get; set; } = new List<Episode>();
+
+        public bool Subscribed { get; set; } = false;
     }
 }
